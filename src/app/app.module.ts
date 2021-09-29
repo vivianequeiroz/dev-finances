0bc6999936427d022f2096aff080f5ec790804d6
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-09-28 22:13:12 -0300

    template: add dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./components/header/header.component";
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
-	declarations: [AppComponent, HeaderComponent],
+	declarations: [AppComponent, HeaderComponent, DashboardComponent],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent],
