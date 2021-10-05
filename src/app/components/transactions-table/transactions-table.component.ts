d427c0fc56ff062ff6f5560f886dbdc677420add
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-10-05 20:59:56 -0300

    chore: transaction table template

--- src/app/components/transactions-table/transactions-table.component.ts
+++ src/app/components/transactions-table/transactions-table.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-transactions-table',
-  templateUrl: './transactions-table.component.html',
-  styleUrls: ['./transactions-table.component.scss']
+	selector: "app-transactions-table",
+	templateUrl: "./transactions-table.component.html",
+	styleUrls: ["./transactions-table.component.scss"],
 })
 export class TransactionsTableComponent implements OnInit {
+	transactionInfos = [
+		{
+			description: "Entradas",
+			value: "R$323,00",
+			date: "20/11/2021",
+			delete:
+				"https://raw.githubusercontent.com/Viviane-Queiroz/dev-finances/main/src/assets/minus.svg",
+		},
+		{
+			description: "Entradas",
+			value: "R$323,00",
+			date: "20/11/2021",
+			delete:
+				"https://raw.githubusercontent.com/Viviane-Queiroz/dev-finances/main/src/assets/minus.svg",
+		},
+		{
+			description: "Entradas",
+			value: "R$323,00",
+			date: "20/11/2021",
+			delete:
+				"https://raw.githubusercontent.com/Viviane-Queiroz/dev-finances/main/src/assets/minus.svg",
+		},
+	];
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
