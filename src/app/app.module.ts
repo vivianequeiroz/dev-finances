b330930a4553fa91498b7b00ef5f280d0b87eb03
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-10-05 18:30:24 -0300

    chore: transactions table components template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { MoneyCardComponent } from './components/shared/money-card/money-card.component';
+import { TransactionsTableComponent } from './components/transactions-table/transactions-table.component';
 
 @NgModule({
-	declarations: [AppComponent, HeaderComponent, DashboardComponent, MoneyCardComponent],
+	declarations: [AppComponent, HeaderComponent, DashboardComponent, MoneyCardComponent, TransactionsTableComponent],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent],
---
+++ src/app/components/transactions-table/transactions-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-transactions-table',
+  templateUrl: './transactions-table.component.html',
+  styleUrls: ['./transactions-table.component.scss']
+})
+export class TransactionsTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
